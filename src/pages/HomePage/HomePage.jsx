4e5e0436c9a08939fedd59e101cf8b2a1ca03d22
MilaIlchenko <147260557+MilaIlchenko@users.noreply.github.com>
2024-10-08 09:36:28 +0200

    add

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,31 +1,19 @@
 import { useEffect, useState } from "react";
-import { getMoviesSearch } from "../../movies-api";
-import { useSearchParams } from "react-router-dom";
-import SearchBar from "../../components/SearchBar/SearchBar";
+import { getMovies } from "../../movies-api";
 import MovieList from "../../components/MovieList/MovieList";
 import Loading from "../../components/Loading/Loading";
 import Error from "../../components/Error/Error";
 
-export default function MoviesPage() {
+export default function HomePage() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  const [searchParams, setSearchParams] = useSearchParams();
-  const query = searchParams.get("query") ?? "";
-
-  const changeQuery = (newFilter) => {
-    setSearchParams({ query: newFilter });
-  };
-
   useEffect(() => {
-    if (query === "") {
-      return;
-    }
     async function fetchMovies() {
       try {
         setIsLoading(true);
-        const data = await getMoviesSearch(query);
+        const data = await getMovies();
         setMovies(data);
       } catch (error) {
         setError(true);
@@ -34,11 +22,11 @@ export default function MoviesPage() {
       }
     }
     fetchMovies();
-  }, [query]);
+  }, []);
 
   return (
     <>
-      <SearchBar value={query} onSubmit={changeQuery} />
+      <h1>Trending today</h1>
       {isLoading && <Loading />}
       {error && <Error />}
       {movies.length > 0 && <MovieList movies={movies} />}
